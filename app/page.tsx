b627153ea374737fe6a8a874c0327fcbdeda10f9
Karitk Gupta <csai23031@glbitm.ac.in>
2025-04-17 07:58:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -146,6 +146,7 @@ export default function Home() {
               Book maid
             </p>
           </div>
+        </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <ServiceCard
